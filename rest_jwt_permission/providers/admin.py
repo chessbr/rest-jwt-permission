fe9f6df7f74519e0f2a09cdf0090efd0b85176f1
vitoriano-ht <diego_vitoriano@epam.com>
2022-03-16 13:45:28 -0300

    wipcraft.

--- rest_jwt_permission/providers/admin.py
+++ rest_jwt_permission/providers/admin.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
-from django.utils.lru_cache import lru_cache
+import django
+
+if django.VERSION < (3, 0):
+    from django.utils.lru_cache import lru_cache
+else:
+    from functools import lru_cache
+
 from django.utils.translation import ugettext_lazy as _
 
 from .base import ScopeProviderBase
